ecb193b96cb3050fd4662943212454da52b12b9b
yang zhang <tianna1121@gmail.com>
2019-09-28 20:11:21 +0800

    add muithemeProvider to provide theme data.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createTheme from "@material-ui/core/styles/createMuiTheme";
 
 // * Pages
 import home from "./pages/home";
@@ -11,20 +13,39 @@ import login from "./pages/login";
 // * Components
 import Navbar from "./components/Navbar";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#00bcd4",
+      dark: "#b22a00",
+      contrastText: "#fff"
+    }
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
