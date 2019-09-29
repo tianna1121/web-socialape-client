5c825bd00d17a55d18eab1df0be647a27025eb5d
yang zhang <tianna1121@gmail.com>
2019-09-29 16:29:23 +0800

    move all theme related stuff to the theme.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import createTheme from "@material-ui/core/styles/createMuiTheme";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./util/theme";
 
 // * Pages
 import home from "./pages/home";
@@ -13,50 +14,7 @@ import login from "./pages/login";
 // * Components
 import Navbar from "./components/Navbar";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: "#33c9dc",
-      main: "#00bcd4",
-      dark: "#008394",
-      contrastText: "#fff"
-    },
-    secondary: {
-      light: "#ff6333",
-      main: "#00bcd4",
-      dark: "#b22a00",
-      contrastText: "#fff"
-    }
-  },
-  typography: {
-    useNextVariants: true
-  },
-  form: {
-    textAlign: "center"
-  },
-  image: {
-    margin: "20px auto 20px auto",
-    maxWidth: "80px"
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto"
-  },
-  textField: {
-    margin: "10px auto 10px auto"
-  },
-  button: {
-    margin: 20,
-    position: "relative"
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10
-  },
-  progress: {
-    position: "absolute"
-  }
-});
+const theme = createMuiTheme(themeFile);
 
 function App() {
   return (
---
+++ src/util/theme.js
@@ -0,0 +1,44 @@
+export default {
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#00bcd4",
+      dark: "#b22a00",
+      contrastText: "#fff"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  form: {
+    textAlign: "center"
+  },
+  image: {
+    margin: "20px auto 20px auto",
+    maxWidth: "80px"
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto"
+  },
+  textField: {
+    margin: "10px auto 10px auto"
+  },
+  button: {
+    margin: 20,
+    position: "relative"
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10
+  },
+  progress: {
+    position: "absolute"
+  }
+};
