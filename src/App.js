095d9f92a391deb294f5e03bc5a90bc4cf2845ae
yang zhang <tianna1121@gmail.com>
2019-09-29 16:33:56 +0800

    add token expire checking with jwt-decode library

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import themeFile from "./util/theme";
+import jwtDecode from "jwt-decode";
 
 // * Pages
 import home from "./pages/home";
@@ -15,6 +16,19 @@ import login from "./pages/login";
 import Navbar from "./components/Navbar";
 
 const theme = createMuiTheme(themeFile);
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = jwtDecode(token);
+  console.log(decodedToken);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 function App() {
   return (
